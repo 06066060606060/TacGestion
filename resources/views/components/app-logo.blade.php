49a1e44b7ef0ad4d6c832d87963eb5b33d9d9397
Mick <xmicky@hotmail.fr>
2025-10-14 18:26:22 +0200

    logo

--- resources/views/components/app-logo.blade.php
+++ resources/views/components/app-logo.blade.php
@@ -2,5 +2,5 @@
    <img src="/favicon.png" alt="Logo" />
 </div>
 <div class="ms-1 grid flex-1 text-start text-sm">
-    <span class="mb-0.5 truncate leading-tight font-semibold">Tac Gestion</span>
+    <span class="mb-0.5 truncate leading-tight font-semibold pt-1">TAC GESTION</span>
 </div>
